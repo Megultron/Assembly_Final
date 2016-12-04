b645dd060d81d164dccba683c2e85a5c3bec747c
cgrove <haroldcgrove@gmail.com>
2016-12-04 14:24:49 -0800

    stuck
    stuck on esi value being 14

--- proj/BlackJack.asm
+++ proj/BlackJack.asm
@@ -13,14 +13,14 @@ INCLUDE Irvine32.inc
 deck BYTE 52 DUP(?)
 shuffled_deck BYTE 52 DUP(?)
 shuffle_range_size BYTE 52
-
+shift_index DWORD ?
 
 
 .code
 
 main PROC
 	call deckgen
-
+	call shuffle
 
 	invoke ExitProcess, 0
 main ENDP
@@ -48,10 +48,43 @@ addtype PROC USES ecx
 	ret
 addtype ENDP
 
-shuffle PROC
-	
+shuffle PROC USES esi
+	mov ecx, 52
+	movzx esi, BYTE PTR [shuffled_deck]
+	call Randomize
+	SHUFFLEL:
+		movzx eax, shuffle_range_size
+		inc eax
+		call RandomRange
+		mov bl, deck[eax * BYTE]
+		mov shuffled_deck[esi * BYTE], bl
+		inc esi
+		dec shuffle_range_size
+		call shiftleft
+		loop SHUFFLEL
 
 	ret
 shuffle ENDP
+
+shiftleft PROC USES ecx edx
+	mov shift_index, eax
+	mov eax, LENGTHOF deck
+	sub eax, shift_index
+	dec eax
+	dec eax
+	mov ecx, eax
+	SHIFTL:
+		mov eax, shift_index
+		inc eax
+		mov bl, deck[eax * BYTE] 
+		mov edx, shift_index
+		mov deck[edx * BYTE], bl
+		mov eax, shift_index
+		inc eax
+		mov shift_index, eax
+		loop SHIFTL
+
+	ret
+shiftleft ENDP
 
 END main
