57aadf6e5bf1a9c0dc187f38b6e5868b721e1544
cgrove <haroldcgrove@gmail.com>
2016-12-11 14:39:21 -0800

    got main loop working
    stuck on ecx val after first loop

--- proj/BlackJack.asm
+++ proj/BlackJack.asm
@@ -6,34 +6,104 @@ TITLE BLACKJACK
 
 INCLUDE Irvine32.inc
 
-
-
-
 .data
 deck BYTE 52 DUP(?)
 shuffled_deck BYTE 52 DUP(?)
 shuffle_range_size BYTE 52
 shift_index DWORD ?
+shuffled_idx BYTE 0
+
 dealer_hand BYTE 10 DUP(?)
 dealer_facedown BYTE ?
 dealer_idx BYTE 0
+
 player_hand BYTE 10 DUP(?)
 player_facedown BYTE ?
 player_idx BYTE 0
 
-
-
+face_up_string BYTE "Face Up: ",0
+face_down_string BYTE "Face Down: ",0
+dealer_faceup_string BYTE "Dealer: ",0
+hit_or_stay BYTE "1. Hit or 2. Stay: ",0
+bust_msg BYTE "You Busted! Press 1 to play again, 0 to exit.", 0
 .code
 
 main PROC
-	call deckgen
-	call shuffle
-	;call DealToHand
 
+call deckgen
+
+
+GameLoop:
+	mov shuffled_idx, 0
+	mov shift_index, 0
+	
+	call shuffle
+	call DealHands
+	mov ecx, 1
+
+MainLoop:
+	inc ecx
+	call DisplayCards
+	call HitOrStay
+	call clrscr
+	call CheckBust
+	cmp eax, 0
+	je MainLoop
+	cmp eax, 1
+	je Bust
+
+Bust:
+	mov edx, OFFSET bust_msg
+	call WriteString
+	call crlf
+	call ReadInt
+	cmp eax, 1
+	je GameLoop
+	cmp eax, 0
+	je ExitGame
+
+ExitGame:
 	invoke ExitProcess, 0
 main ENDP
 
+CheckBust PROC
+	mov al, player_facedown
+	mov esi, 0
+	mov ecx, LENGTHOF player_hand
+	SumLoop:
+		add al, player_hand[esi]
+		inc esi
+		cmp eax, 21
+		ja Bust
+		loop SumLoop
+		mov eax, 0
+		ret
+	Bust:
+		mov eax, 1
+		ret
+		
 
+ret
+CheckBust ENDP
+
+DealHands PROC
+
+	mov edx, 0
+	mov ebx, 0
+	call DealToHand ;Player Faceup
+	mov edx, 0
+	mov ebx, 1
+	call DealToHand ;Dealer Faceup
+
+	mov edx, 1
+	mov ebx, 0
+	call DealToHand ;Player Facedown
+	mov edx, 1
+	mov ebx, 1
+	call DealToHand ;Dealer Facedown
+
+ret
+DealHands ENDP
 
 deckgen PROC
 	mov bl, 1
@@ -56,7 +126,7 @@ addtype PROC USES ecx
 	ret
 addtype ENDP
 
-shuffle PROC USES esi
+shuffle PROC USES esi 
 	mov ecx, 52
 	movzx esi, BYTE PTR [shuffled_deck]
 	call Randomize
@@ -96,6 +166,7 @@ shiftleft PROC USES ecx edx
 shiftleft ENDP
 
 DealToHand PROC ;edx bool var to indicate face up or face down if true, ecx true if dealing to dealer
+	movzx esi, shuffled_idx
 	cmp edx, 1
 	je FACEDOWN
 	cmp edx, 0
@@ -103,27 +174,88 @@ DealToHand PROC ;edx bool var to indicate face up or face down if true, ecx true
 
 
 	FACEDOWN:
-		cmp ecx, 1
+		cmp ebx, 1
 		je DEALER_DOWN
-		cmp ecx, 0
+		cmp ebx, 0
 		je PLAYER_DOWN
 
 		DEALER_DOWN:
-
+			mov cl, shuffled_deck[esi]
+			mov dealer_facedown, cl
+			inc shuffled_idx
+			ret
 		PLAYER_DOWN:
-
+			mov cl, shuffled_deck[esi]
+			mov player_facedown, cl
+			inc shuffled_idx
+			ret
 
 	FACEUP:
-		cmp ecx, 1
+		cmp ebx, 1
 		je DEALER_UP
-		cmp ecx, 0
+		cmp ebx, 0
 		je PLAYER_UP
 
 		DEALER_UP:
-
+			movzx eax, dealer_idx
+			mov cl, shuffled_deck[esi]
+			mov dealer_hand[eax], cl
+			inc shuffled_idx
+			inc dealer_idx
+			ret
 		PLAYER_UP:
-	
+			movzx eax, player_idx
+			mov cl, shuffled_deck[esi]
+			mov player_hand[eax], cl
+			inc shuffled_idx
+			inc player_idx
+			ret
 
 DealToHand ENDP
+
+DisplayCards PROC
+
+	mov edx,OFFSET face_up_string
+	call WriteString
+	mov esi,0
+	mov ecx, LENGTHOF player_hand
+	PlayerHand:
+		mov al, player_hand[esi]
+		call WriteInt
+		inc esi
+		loop PlayerHand
+
+	call crlf
+	mov edx, OFFSET face_down_string
+	call WriteString
+	mov al, player_facedown
+	call WriteInt
+	call crlf
+
+	mov edx, OFFSET dealer_faceup_string
+	call WriteString
+	mov al, dealer_hand[0]
+	call WriteInt
+	call crlf
+
+	ret
+DisplayCards ENDP
+
+HitOrStay PROC
+
+	mov edx,OFFSET hit_or_stay
+	call WriteString
+	call ReadInt
+	cmp eax,1
+	je HIT
+	ret
+
+	HIT:
+		mov edx,0
+		mov ebx,0
+		call DealToHand
+		ret
+
+HitOrStay ENDP
 
 END main
